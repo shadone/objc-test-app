b7c700f9ce45f317c7f0067e268df32cc803e09b
Denis Dzyubenko <denis@ddenis.info>
2024-04-11 13:19:13 +0200

    Added local SPM package and try to use it from Objective C code

---
+++ Modules/Sources/Modules/Modules.swift
@@ -0,0 +1,12 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+@objc
+public class Foobar: NSObject {
+    public override init() { }
+
+    @objc
+    public func tada() -> String { "hello world" }
+}
